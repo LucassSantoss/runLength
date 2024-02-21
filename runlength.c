16afb5d2b10ff76510031f42cf900bcd008a5693
LucassSantoss <lucascross.pereirasantos@gmail.com>
2024-02-20 22:38:30 -0300

    open files

--- runlength.c
+++ runlength.c
@@ -5,7 +5,16 @@
 void main(int qtdArgumentos, char *nomeArquivos[])
 {
   setlocale(LC_ALL, "Portuguese");
-
   if (qtdArgumentos != 3) // nome do programa, arquivo de entrada, arquivo de saída
     printf("Para usar o programa, use: ./%s arquivo_entrada arquivo_saída\n", nomeArquivos[0]);
+
+  // Declarando variáveis dos arquivos
+  FILE *arqEntrada = fopen(nomeArquivos[1], "r"); // abre apenas para leitura
+  FILE *arqSaida = fopen(nomeArquivos[2], "w");   // abre para escrita
+
+  if (arqEntrada == NULL)
+    printf("Erro ao abrir o arquivo de entrada.\n");
+
+  if (arqSaida == NULL)
+    printf("Erro ao abrir o arquivo de saída.\n");
 }
