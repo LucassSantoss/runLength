b87ac734ea3d2a635016ba3e45046bce3a56f3c0
LucassSantoss <lucascross.pereirasantos@gmail.com>
2024-02-20 22:37:38 -0300

    passes files by parameter

--- runlength.c
+++ runlength.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <locale.h>
 
-void main()
+void main(int qtdArgumentos, char *nomeArquivos[])
 {
   setlocale(LC_ALL, "Portuguese");
+
+  if (qtdArgumentos != 3) // nome do programa, arquivo de entrada, arquivo de saída
+    printf("Para usar o programa, use: ./%s arquivo_entrada arquivo_saída\n", nomeArquivos[0]);
 }
