cf1705c7b8bfceea83a9e825a05f0fe9ba1b825d
LucassSantoss <lucascross.pereirasantos@gmail.com>
2024-02-20 22:46:30 -0300

    changed the name of a variable

--- runlength.c
+++ runlength.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include <locale.h>
 
-void main(int qtdArgumentos, char *nomeArquivos[])
+void main(int qtdArgumentos, char *argumentos[])
 {
   setlocale(LC_ALL, "Portuguese");
   if (qtdArgumentos != 3) // nome do programa, arquivo de entrada, arquivo de saída
-    printf("Para usar o programa, use: ./%s arquivo_entrada arquivo_saída\n", nomeArquivos[0]);
+    printf("Para usar o programa, use: ./%s arquivo_entrada arquivo_saída\n", argumentos[0]);
 
   // Declarando variáveis dos arquivos
-  FILE *arqEntrada = fopen(nomeArquivos[1], "r"); // abre apenas para leitura
-  FILE *arqSaida = fopen(nomeArquivos[2], "w");   // abre para escrita
+  FILE *arqEntrada = fopen(argumentos[1], "r"); // abre apenas para leitura
+  FILE *arqSaida = fopen(argumentos[2], "w");   // abre para escrita
 
   if (arqEntrada == NULL)
     printf("Erro ao abrir o arquivo de entrada.\n");
